1d6997c8e6ddc7fb7b75e65cf771eccb99591bdd
Derek McGee <dmcgee2001@gmail.com>
2023-11-03 13:38:50 -0400

    adds functions to for body, header, footer, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,65 @@
+function Header() {
+  return (
+    <header>
+      <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
+    </header>
+  );
+}
+function PostsNew() {
+  return (
+    <div id="posts-new">
+      <h1>New post</h1>
+      <div>
+        Title: <input type="text" name="" id="" />
+      </div>
+      <div>
+        Body: <input type="text" name="" id="" />
+      </div>
+      <div>
+        Image: <input type="image" src="" alt="" />
+      </div>
+    </div>
+  );
+}
+
+function PostsIndex() {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <div className="posts">
+        <h3>Blog Post 1</h3>
+        <img src="" alt="" />
+        <p>This is the body of the first blog post.</p>
+      </div>
+      <div>
+        <h3>Blog Post 2</h3>
+        <img src="" alt="" />
+        <p>This is the body of the second blog post.</p>
+      </div>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 20XX</p>
+    </footer>
+  );
+}
+function Content() {
+  return (
+    <div>
+      <PostsNew />
+      <PostsIndex />
+    </div>
+  );
+}
 function App() {
   return (
     <div>
-      <header>
-        <a href="#">Home</a> | <a href="#posts-index">All posts</a> | <a href="#posts-new">New post</a>
-      </header>
-
-      <div id="posts-new">
-        <h1>New post</h1>
-        <div>
-          Title: <input type="text" name="" id="" />
-        </div>
-        <div>
-          Body: <input type="text" name="" id="" />
-        </div>
-        <div>
-          Image: <input type="image" src="" alt="" />
-        </div>
-      </div>
-
-      <div id="posts-index">
-        <h1>All posts</h1>
-        <div>
-          <h3>Blog Post 1</h3>
-          <img src="" alt="" />
-          <p>This is the body of the first blog post.</p>
-        </div>
-        <div>
-          <h3>Blog Post 2</h3>
-          <img src="" alt="" />
-          <p>This is the body of the second blog post.</p>
-        </div>
-      </div>
-
-      <footer>
-        <p>Copyright 20XX</p>
-      </footer>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
