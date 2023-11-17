3b645907bc4becc3ac9ea8a1aa2da5a2e9a01152
Derek McGee <dmcgee2001@gmail.com>
2023-11-17 12:59:15 -0500

    changes home page link to an actual link instead of an achor tag

--- src/Header.jsx
+++ src/Header.jsx
@@ -21,9 +21,9 @@ export function Header() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#posts-index">
